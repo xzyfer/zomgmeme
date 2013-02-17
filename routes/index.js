6555a2bcf20548e379b35e00e8666fadff949e6d
xzyfer <xzyfer@gmail.com>
2013-02-17 20:42:37 +1000

    Add imgur support to the node api

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var
   Canvas = require('canvas'),
   Image = Canvas.Image,
   im = require('imagemagick'),
-  generator = require('../zomgmeme-generator');
+  generator = require('../zomgmeme-generator'),
+  imgur = require('imgur-api')('46137a3a008f4960d3076429229d4545');
 
 module.exports = function (_app) {
     app = _app;
@@ -17,6 +18,7 @@ controller.index = function(req, res) {
     meme = req.param('meme'),
     top = req.param('top'),
     bottom = req.param('bottom'),
+    publish = req.param('publish'),
     imagePath = __dirname + '/../public/images/characters/' + meme + '.png';
 
   if(!fs.existsSync(imagePath)) {
@@ -33,8 +35,19 @@ controller.index = function(req, res) {
     img.src = fs.readFileSync(imagePath);
     var meme = generator(canvas, img, top, bottom);
 
-    res.set('Content-Type', 'text/plain');
-    res.send(meme.toBuffer());
+    if(publish) {
+      meme.toDataURL('image/png', function(err, data) {
+        if (err) throw err;
+        imgur.upload(data.replace(/data:image\/png;base64,/, ''), function(err, data) {
+          if (err) throw err;
+          res.set('Content-Type', 'text/json');
+          res.send(data.upload.links.original);
+        });
+      });
+    } else {
+      res.set('Content-Type', 'text/plain');
+      res.send(meme.toBuffer());
+    }
   });
 
 };
